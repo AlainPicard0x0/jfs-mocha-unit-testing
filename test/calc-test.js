c60034c8251ff6e39a29e5f5370a80a4ec0940c6
Rita Frias <rfrias8@gmail.com>
2021-09-16 11:20:38 -0500

    add mocha tests

--- test/calc-test.js
+++ test/calc-test.js
@@ -17,4 +17,44 @@ describe('Calc', () => {
         assert.ok(answer===undefined);
       });
     });
+    describe('.subtract', () => {
+      it('returns value 2 minus value 1', () => {
+        //setup
+        const calc = new Calc();
+        //exercise
+        const answer = calc.subtract(6,3);
+        //verify
+        assert.ok(answer===-3);
+      });
+    });
+    describe('.multiply', () => {
+      it('returns the product of value 1 * value 2', () => {
+        //setup
+        const calc = new Calc();
+        //exercise
+        const answer = calc.multiply(6,3);
+        //verify
+        assert.ok(answer===18);
+      });
+    });
+    describe('.divide', () => {
+      it('returns value 1 divided by value 2', () => {
+        //setup
+        const calc = new Calc();
+        //exercise
+        const answer = calc.divide(10,5);
+        //verify
+        assert.ok(answer===2);
+      });
+    });
+    describe('.convertToF', () => {
+      it('returns celsius converted to Fahrenheit', () => {
+        //setup
+        const calc = new Calc();
+        //exercise
+        const answer = calc.convertToF(32);
+        //verify
+        assert.ok(answer===89.6);
+      });
+    });
   });
